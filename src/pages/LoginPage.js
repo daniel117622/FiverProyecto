10d5066e80850001e452c02aff42770b0fc294bb
daniel117622 <dan5jul@hotmail.com>
2023-02-22 16:59:54 -0600

    React Router

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function LoginPage()
+{
+    const [datos, setdatos] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleChange = (e) => {
+
+        setdatos({...datos,[e.target.name] : e.target.value});
+        console.log(datos);
+    }
+
+    const handleSubmit = (e) => 
+    {
+        if (datos.username === "daniel117622" && datos.password === "")
+        {
+            alert(1);
+        }
+    }
+
+    return (
+        <div>
+            <form method="GET">
+                <label>Usuario:</label>
+                <input type="text" name="username" onChange={handleChange}></input>
+                <br/>
+                <label>Password:</label>
+                <input type="text" name="password" onChange={handleChange}></input>  
+                <br/>
+                <button onClick={handleSubmit}> Login </button>              
+            </form>
+        </div>
+    );
+}
+
+export default LoginPage;
